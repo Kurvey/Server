25a59cd3e1b38f875555e2ba0af786a40fc942e5
HoYoon-Lee <happylhy0630@naver.com>
2022-08-20 13:32:23 +0900

    :tada: 프로젝트 세팅

---
+++ src/main/java/com/kurvey/u_life_kurly/ULifeKurlyApplication.java
@@ -0,0 +1,13 @@
+package com.kurvey.u_life_kurly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ULifeKurlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ULifeKurlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kurvey/u_life_kurly/ULifeKurlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurvey.u_life_kurly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ULifeKurlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
