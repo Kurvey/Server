ba21528a92525cd40e50f14564a4cf796373205c
HoYoon-Lee <42956464+HoYoon-Lee@users.noreply.github.com>
2022-08-22 15:15:55 +0900

    Create deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/u-life-kurly
+
+echo "> Copy build files"
+
+cp $REPOSITORY/server/*.jar $REPOSITORY/
+
+echo "> Checking pid of currently running application"
+
+CURRENT_PID=$(pgrep -fl server | grep java | awk '{print $1}')
+
+echo "Current application's pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> The application is not currently running, so it will not be shut down."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> Deploy new application"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> chmod + x $JAR_NAME"
+
+chmod +x $JAR_NAME
+
+echo "> Execute $JAR_NAME "
+
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
