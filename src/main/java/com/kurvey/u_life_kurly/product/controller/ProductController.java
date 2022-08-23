7e34e25f3dc9b8473df6ab8d2c07ec6cfe6ddd14
HoYoon-Lee <happylhy0630@naver.com>
2022-08-23 17:05:21 +0900

    :art: API 메소드와 경로 수정

--- src/main/java/com/kurvey/u_life_kurly/product/controller/ProductController.java
+++ src/main/java/com/kurvey/u_life_kurly/product/controller/ProductController.java
@@ -6,24 +6,24 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
 
 
 @Api(tags = "검색")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/product")
 public class ProductController {
     private final ProductService productService;
 
-    @ApiOperation(value = "상품 검색")
-    @PostMapping("searchresult/products")
-    public ResponseEntity searchproducts(@RequestBody Map<String, String> body){
-        String keyword = body.get("keyword");
+    @ApiOperation(value = "상품 검색 API")
+    @GetMapping("{keyword}")
+    public ResponseEntity searchProducts(@PathVariable String keyword){
         List<CategoryDto> products = productService.searchProducts(keyword);
         return ResponseEntity.ok(products);
     }
