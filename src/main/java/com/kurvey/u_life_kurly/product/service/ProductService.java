6503f46ed88c2cf1d33489e5c39eab835c87afc6
HoYoon-Lee <happylhy0630@naver.com>
2022-08-23 18:53:56 +0900

    :art: 검색 API 반환값 양식 수정

--- src/main/java/com/kurvey/u_life_kurly/product/service/ProductService.java
+++ src/main/java/com/kurvey/u_life_kurly/product/service/ProductService.java
@@ -1,24 +1,25 @@
 package com.kurvey.u_life_kurly.product.service;
 
-import com.kurvey.u_life_kurly.product.dto.CategoryDto;
 import com.kurvey.u_life_kurly.product.dto.ProductDto;
+import com.kurvey.u_life_kurly.product.dto.ProductsByCategoryDto;
+import com.kurvey.u_life_kurly.product.entity.Category;
 import com.kurvey.u_life_kurly.product.entity.Product;
 import com.kurvey.u_life_kurly.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-
 public class ProductService {
 
     private final ProductRepository productRepository;
-
-
 
     @Transactional
     public void saveProduct(ProductDto productDto) {
@@ -27,36 +28,32 @@ public class ProductService {
     }
 
     @Transactional
-    public List<CategoryDto> searchProducts(String keyword) {
+    public List<ProductsByCategoryDto> searchProducts(String keyword) {
         List<Product> products = productRepository.findAllByNameContaining(keyword);
-        List<CategoryDto> categoryDtos = new ArrayList<>();
+        List<ProductsByCategoryDto> productsByCategoryDtoList = new ArrayList<>();
 
-        if(products.isEmpty()) return categoryDtos;
+        if(products.isEmpty()) return productsByCategoryDtoList;
 
-        Map<String, List<Product>> productsByCategory = products.stream()
-                .collect(Collectors.groupingBy(p -> p.getCategory().getName()));
-        categoryDtos = productsByCategory.entrySet().stream()
-                .map(e -> new CategoryDto(e.getKey(), e.getValue().stream()
+        Map<Category, List<Product>> productsByCategory = products.stream()
+                .collect(Collectors.groupingBy(Product::getCategory));
+        productsByCategoryDtoList = productsByCategory.entrySet().stream()
+                .map(e -> new ProductsByCategoryDto(e.getKey().getId(), e.getKey().getName(), e.getValue().stream()
                         .map(this::convertEntityToDto)
                         .collect(Collectors.toList())))
-                .sorted(Comparator.comparing(categoryDto -> categoryDto.getProducts().size(), Comparator.reverseOrder()))
+                .sorted(Comparator.comparing(pbc -> pbc.getProducts().size(), Comparator.reverseOrder()))
                 .collect(Collectors.toList());
 
-        return categoryDtos;
+        return productsByCategoryDtoList;
     }
-
-
 
     private ProductDto convertEntityToDto(Product product) {
         return ProductDto.builder()
                 .id(product.getId())
                 .deliveryType(product.getDeliveryType())
+                .productName(product.getName())
                 .cost(product.getCost())
                 .description(product.getDescription())
                 .build();
     }
-
-
-
-    }
+}
 
