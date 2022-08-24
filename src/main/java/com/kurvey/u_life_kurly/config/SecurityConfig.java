672075f188a2f33a6e781600825283e4cd3b2a71
HoYoon-Lee <happylhy0630@naver.com>
2022-08-24 10:58:03 +0900

    :lock: CORS: @EnableWebMvc 어노테이션 추가

--- src/main/java/com/kurvey/u_life_kurly/config/SecurityConfig.java
+++ src/main/java/com/kurvey/u_life_kurly/config/SecurityConfig.java
@@ -13,9 +13,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
+@EnableWebMvc
 @EnableWebSecurity
 public class SecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
