f6a5322f16565343a9d434617b157f7d4708f919
HoYoon-Lee <happylhy0630@naver.com>
2022-08-29 21:47:36 +0900

    :sparkles: 설문조사 응답 조회 API

--- src/main/java/com/kurvey/u_life_kurly/user/Repository/LifeStyleAnswerRepository.java
+++ src/main/java/com/kurvey/u_life_kurly/user/Repository/LifeStyleAnswerRepository.java
@@ -2,8 +2,12 @@ package com.kurvey.u_life_kurly.user.repository;
 
 import com.kurvey.u_life_kurly.user.entity.LifeStyleAnswer;
 import com.kurvey.u_life_kurly.user.entity.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface LifeStyleAnswerRepository extends JpaRepository<LifeStyleAnswer, Long> {
     void deleteByUser(User user);
+    List<LifeStyleAnswer> findAllByUser(User user, Sort sort);
 }
